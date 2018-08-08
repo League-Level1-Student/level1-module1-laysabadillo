f9ef9d63f11393d56d39de12d8e1ce401a9f2357
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 16:50:35 -0700

    SeaCreatureRunner incomplete

--- src/SeaCreatureRunner.java
+++ src/SeaCreatureRunner.java
@@ -2,6 +2,11 @@
 public class SeaCreatureRunner {
 	public static void main(String args[]) {
 		
+		SeaCreature spongebob = new SeaCreature("spongebob");
+		spongebob.eat();
+		spongebob.laugh();
+		
+		
 		
 	}
 
